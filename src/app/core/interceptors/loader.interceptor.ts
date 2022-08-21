c88a8cd7168280df060f1808775cb37106d19eb3
Nofishtou <damnedbyhimself@gmail.com>
2022-08-21 09:41:42 +0300

    fix: fixed naming

--- src/app/core/interceptors/loader.interceptor.ts
+++ src/app/core/interceptors/loader.interceptor.ts
@@ -6,30 +6,30 @@ import {
   HttpInterceptor,
   HttpResponse
 } from '@angular/common/http';
-import { Observable, finalize, catchError, map } from 'rxjs';
+import { Observable, catchError, map } from 'rxjs';
 import { LoaderService } from '../services/loader.service';
 
 @Injectable()
 export class LoaderInterceptor implements HttpInterceptor {
-  constructor(private _loading: LoaderService) {}
+  constructor(private loaderService: LoaderService) {}
 
   intercept(
     request: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    this._loading.setLoading(true, request.url);
+    this.loaderService.setLoading(true, request.url);
     return next
       .handle(request)
       .pipe(
         catchError((err) => {
-          this._loading.setLoading(false, request.url);
+          this.loaderService.setLoading(false, request.url);
           return err;
         })
       )
       .pipe(
         map((evt: any) => {
           if (evt instanceof HttpResponse) {
-            this._loading.setLoading(false, request.url);
+            this.loaderService.setLoading(false, request.url);
           }
           return evt;
         })
